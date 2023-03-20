911ec3c260e8a31c5929a0f821d81f713c79c9ee
Hambulus <urietoemmanuel@gmail.com>
2023-03-20 07:53:50 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * putchar - Outputs texts stored in a variable
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char alphbt = 'a';
+
+	while (alphbt < 'z')
+	{
+		if (alphbt != 'q' && alphbt !='e')
+		putchar(alphbt);
+		alphbt++;
+	}
+	putchar('\n');
+	return (0);
+}
