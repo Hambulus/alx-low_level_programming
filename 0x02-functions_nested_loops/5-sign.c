f4247083f14c68209d049248db5efbfe72e1b973
Hambulus <urietoemmanuel@gmail.com>
2023-03-28 14:13:09 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int print_sign(int n)
+{
+	int a = 0;
+	int b = '+';
+	int c = '-';
+
+	if (n > a)
+		_putchar(b);
+	if (n == a)
+		_putchar(a);
+	if (n < a)
+		_putchar(c);
+	return (0);
+}
