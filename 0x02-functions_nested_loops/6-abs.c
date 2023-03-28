55bd2dbdb342ed0d0d320fa77bb7f4b260a595fb
Hambulus <urietoemmanuel@gmail.com>
2023-03-28 15:54:31 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * abs - Prints absolute values
+  *
+  * printf - prints output from variables
+  *
+  * Return: Always 0 (Success)
+  */
+int _abs(int)
+{
+	int a;
+
+	a = _abs(-3);
+	printf("%d\n", a);
+	return (0);
+}
