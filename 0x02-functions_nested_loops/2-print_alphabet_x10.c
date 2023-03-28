2301e10813363a6cb2b06527c13b47c902fef4b1
Hambulus <urietoemmanuel@gmail.com>
2023-03-27 22:27:16 -0700

    print x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - Entry point
+  *
+  * _putchar - print out character
+  *
+  * Return: Always 0 (Success)
+  */
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+	int n = 0;
+
+	while (n < 10)
+	{
+		 while (alpha <= 'z')
+		 {
+			 _putchar(alpha);
+			 alpha++;
+		 }
+		 n++;
+	}
+        _putchar('\n');
+}
