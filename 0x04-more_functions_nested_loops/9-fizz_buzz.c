3e5e8edd12a28a168723364506dfdc5c29e5da8a
Hambulus <urietoemmanuel@gmail.com>
2023-04-02 13:27:21 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3) == 0)
-			printf("fizz ");
+			printf("fizz");
 		if ((n % 5) == 0)
 			printf("buzz");
 		else if (((n % 3) == 0) && ((n % 5) == 0))
