1e4e38da521fdb580ec8b4be74ceb7169f38b471
Hambulus <urietoemmanuel@gmail.com>
2023-04-05 12:42:31 -0700

    resert to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * reset_to_98 - function resets variable to 98
+  *
+  * _putchar - outputs character from function
+  *
+  * Return: Always 0 (Success)
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
