2c3ffe5e411f36b66086139483c6cf69ef3704c9
Hambulus <urietoemmanuel@gmail.com>
2023-04-05 13:00:20 -0700

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * swap_int - function swaps the output of two variables
+  *
+  * _putchar - outputs characters from a function
+  *
+  * Return: Always 0 (Success)
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
