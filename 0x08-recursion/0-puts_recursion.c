71ded9e5d4f93a812132420d3237870816d825ca
Hambulus <urietoemmanuel@gmail.com>
2023-06-07 18:45:57 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * _puts_recursion - function that prints a sting
+ * *s: Parameter to the function
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
