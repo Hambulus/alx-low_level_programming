c7c18bac25d78391880c15ece8a05fa7bcf49ac3
Hambulus <urietoemmanuel@gmail.com>
2023-06-11 02:57:45 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,13 +4,15 @@
 
 /**
  * main - a function that prints its own name
+ * @argc: counts the number os arguments
+ * @argv: Show the index or position of ever argument
  * Return: Always 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
-	
-	for(i = 0; i < argc; i++)
+
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
 }
