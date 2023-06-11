672ed523a72c33b9a96c60e3b69b67176b81d925
Hambulus <urietoemmanuel@gmail.com>
2023-06-11 02:48:41 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - a function that prints its own name
+ * Return: Always 0
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+	
+	for(i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
